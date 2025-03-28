5a5eee3d3855f0836526b1246d333ab1b691694b
Yuchen Liu <yl6448@nyu.edu>
2025-03-28 00:59:16 -0400

    change accelerate

--- lora_training/use_accelerator.zsh
+++ lora_training/use_accelerator.zsh
@@ -6,17 +6,16 @@ accelerate launch train_dreambooth_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of sks dog" \
+  --instance_prompt="cartoon characters and machinery setups" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
   --checkpointing_steps=100 \
-  --learning_rate=1e-4 \
+  --learning_rate=1e-3 \
   --report_to="wandb" \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --max_train_steps=500 \
-  --validation_prompt="A photo of sks dog in a bucket" \
+  --validation_prompt="two pandas using python to crunch numbers" \
   --validation_epochs=50 \
   --seed="0" \
-  --push_to_hub
